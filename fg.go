7f82c8977660981d9abf3349294d63315f28aa3c
George Micros <gmicros@saturn.vavb03.gd-ais.com>
2016-08-25 16:38:49 -0400

    make forloop parallel maybe...

--- fg.go
+++ fg.go
@@ -2,10 +2,37 @@ package main
 
 import "os"
 import "fmt"
+import "time"
 import "image"
 import "image/gif"
 import "image/color"
 import "image/color/palette"
+
+	type pnt struct {
+		re float64
+		im float64
+		level int
+	}
+
+func doSomething(i int, j int, cmplPl [][]pnt, x0 float64, y0 float64, k int, img *image.Paletted)(pnt) {
+	if(cmplPl[i][j].level == 0 ) {
+					//x0 = float64(minX) + float64(i)*float64(maxX - minX)/float64(width-1)
+					//y0 = float64(maxY) - float64(j)*float64(maxY - minY)/float64(height-1)
+					temp := cmplPl[i][j].re * cmplPl[i][j].re - cmplPl[i][j].im * cmplPl[i][j].im + x0
+					cmplPl[i][j].im = 2 * cmplPl[i][j].re * cmplPl[i][j].im + y0
+					cmplPl[i][j].re = temp 				
+					if(cmplPl[i][j].re*cmplPl[i][j].re + cmplPl[i][j].im*cmplPl[i][j].im > 4){
+						cmplPl[i][j].level = k + 1
+						img.Set(i, j, color.RGBA{ uint8(7*cmplPl[i][j].level), uint8(7*cmplPl[i][j].level), uint8(55*cmplPl[i][j].level), 255})
+					}
+				} else {
+					img.Set(i, j, color.RGBA{ uint8(7*cmplPl[i][j].level), uint8(7*cmplPl[i][j].level), uint8(55*cmplPl[i][j].level), 255})
+
+				}
+
+	return cmplPl[i][j]
+}
+
 
 func main() {
 	fmt.Println("Generating fractal gif")
@@ -15,17 +42,11 @@ func main() {
 	var width, height, iter int = 600, 600, 20
 	var minX, maxX, minY, maxY = -2, -1, 0, 1
 
-	type pnt struct {
-		re float64
-		im float64
-		level int
-	}
-
 	cmplPl := make([][]pnt, width)	
-	for _, xi := range cmplPl {
-		xi = make([]pnt, height)	
+	for i := range cmplPl {
+		cmplPl[i] = make([]pnt, height)	
 	}
-	
+		
 
 	levels := make([][]int, width)
 	reVals := make([][]float64, width)
@@ -42,8 +63,10 @@ func main() {
 	var delays []int
 
 	var x0, y0 float64
+
+	start := time.Now()
+/*
 	var temp float64
-	
 	for k := 0; k < iter; k++ {
 		img := image.NewPaletted(image.Rect(0, 0, width, height), palette.Plan9)
 		images = append(images, img)
@@ -67,6 +90,29 @@ func main() {
 			} 
 		}
 	}
+*/
+	elapsed := time.Since(start)
+	fmt.Println(elapsed)
+	next := time.Now()	
+	
+	for k := 0; k < iter; k++ {
+		img := image.NewPaletted(image.Rect(0, 0, width, height), palette.Plan9)
+		images = append(images, img)
+		delays = append(delays, 80) 
+		for i := 0; i < width; i++ {
+			x0 = float64(minX) + float64(i)*float64(maxX - minX)/float64(width-1)
+			for j := 0; j < height; j++ {
+				y0 = float64(maxY) - float64(j)*float64(maxY - minY)/float64(height-1)
+				go func(i int, j int, cmplPl [][]pnt, x0 float64, y0 float64, k int, img *image.Paletted) {
+					cmplPl[i][j] = doSomething(i, j, cmplPl, x0, y0, k, img);
+				}(i, j, cmplPl, x0, y0, k, img);
+				
+			} 
+		}
+	}
+
+	elapsed = time.Since(next)
+	fmt.Println(elapsed)
 	f, _ := os.OpenFile("mandle.gif", os.O_WRONLY|os.O_CREATE, 0600)
 	defer f.Close()
 	gif.EncodeAll(f, &gif.GIF{
